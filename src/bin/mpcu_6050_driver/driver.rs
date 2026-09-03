3fc839572d7a703806ab0e1997f2cac06683999c
SomeFlyingThing <306498559+SomeFlyingThing@users.noreply.github.com>
2026-09-03 10:17:28 +0100

    first configuration stepes

--- src/bin/mpcu_6050_driver/driver.rs
+++ src/bin/mpcu_6050_driver/driver.rs
@@ -1,14 +1,24 @@
 //! Register documentation: <https://dfimg.dfrobot.com/enshop/image/data/SEN0142/RM-MPU-6000A.pdf>
 
+use core::marker::PhantomData;
+
+use embedded_hal::i2c::I2c;
+use esp_hal::i2c;
+
 const WHO_AM_I: u8 = 0x75;
 const DEVICE_ADDR: u8 = 0x68;
 const PWR_MGMT_1: u8 = 0x6B;
 const PWR_MGMT_2: u8 = 0x6C;
+const FIFO_COUNT_H: u8 = 0x72;
+const FIFO_COUNT_L: u8 = 0x73;
+const MOT_THR: u8 = 0x1F;
+const GYRO_CONFIG: u8 = 0x1B;
 
 pub enum Error<E> {
     WhoAmI,
     I2c(E),
 }
+
 impl<E> From<E> for Error<E> {
     fn from(value: E) -> Self {
         Error::I2c(value)
@@ -27,6 +37,10 @@ where
 
     Ok(())
 }
+pub enum Capabilities {
+    Accelerometer,
+    Gyroscope,
+}
 
 pub enum WakeUpFrequency {
     VeryLow,
@@ -35,21 +49,31 @@ pub enum WakeUpFrequency {
     High,
 }
 
-pub struct Mpu6050<BUS>
+pub struct OFF;
+pub struct ON;
+pub struct CapabilitiesSetted;
+
+pub struct Mpu6050<BUS, State>
 where
     BUS: embedded_hal::i2c::I2c,
 {
     i2c: BUS,
+    capabilities: Option<Capabilities>,
+    _data: PhantomData<State>,
 }
 
-impl<BUS> Mpu6050<BUS>
+impl<BUS, OFF> Mpu6050<BUS, OFF>
 where
     BUS: embedded_hal::i2c::I2c,
 {
     pub fn init(mut i2c: BUS) -> Result<Self, Error<BUS::Error>> {
         hello(&mut i2c)?;
 
-        Ok(Self { i2c })
+        Ok(Self {
+            i2c,
+            capabilities: None,
+            _data: PhantomData,
+        })
     }
 
     ///This register allows the user to configure the frequency of wake-ups in Accelerometer Only Low
@@ -62,7 +86,7 @@ where
     ///     (ii) Set SLEEP bit to 0
     ///     (iii) Set TEMP_DIS bit to 1
     ///     (iv) Set STBY_XG, STBY_YG, STBY_ZG bits to 1
-    pub fn leave_sleep_mode(&mut self) -> Result<(), Error<BUS::Error>> {
+    fn leave_sleep_mode(&mut self) -> Result<(), Error<BUS::Error>> {
         let mut red = [0u8, 1];
         self.i2c.write_read(DEVICE_ADDR, &[PWR_MGMT_1], &mut red)?;
 
@@ -94,7 +118,7 @@ where
     /// 1               5 Hz
     /// 2               20 Hz
     /// 3               40 Hz
-    pub fn set_wake_up_frequency(&mut self, frequency: WakeUpFrequency) -> Result<(), Error<BUS::Error>> {
+    fn set_wake_up_frequency(&mut self, frequency: WakeUpFrequency) -> Result<(), Error<BUS::Error>> {
         let frequency = get_wake_up_frequency_bit(frequency);
 
         let mut red = [0u8; 1];
@@ -108,6 +132,14 @@ where
         self.i2c.write(DEVICE_ADDR, &[PWR_MGMT_2, byte])?;
 
         Ok(())
+    }
+
+    fn read_fifo_count(&mut self) -> Result<u16, Error<BUS::Error>> {
+        let mut red = [0u8; 2];
+
+        self.i2c.write_read(DEVICE_ADDR, &[FIFO_COUNT_H], &mut red)?;
+
+        Ok(u16::from_be_bytes([red[0], red[1]]))
     }
 }
 
@@ -124,6 +156,30 @@ const fn get_wake_up_frequency_bit(frequency: WakeUpFrequency) -> u8 {
         WakeUpFrequency::High => 3,
     }
 }
+
+impl<BUS: I2c, OFF> Mpu6050<BUS, OFF> {
+    pub fn start(mut self, frequency: WakeUpFrequency) -> Result<Mpu6050<BUS, ON>, Error<BUS::Error>> {
+        self.leave_sleep_mode()?;
+        self.set_wake_up_frequency(frequency)?;
+
+        Ok(Mpu6050::<BUS, ON> {
+            i2c: self.i2c,
+            capabilities: None,
+            _data: PhantomData,
+        })
+    }
+}
+
+impl<BUS: I2c, ON> Mpu6050<BUS, ON> {
+    pub fn set_capabilities(self, capabilities: Capabilities) -> Mpu6050<BUS, CapabilitiesSetted> {
+        Mpu6050::<BUS, CapabilitiesSetted> {
+            _data: PhantomData,
+            capabilities: Some(capabilities),
+            i2c: self.i2c,
+        }
+    }
+}
+//after configure mesuraments ranges GYRO_CONFIG ACCEL_CONFIG
 // TODO continue on page 44
 
 #[cfg(test)]
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
 #![no_std]
 
-#[path = "bin/mpcu_6050_driver/driver.rs"]
+#[path = "bin/mpcu_6050_driver/mod.rs"]
 pub mod mpu_6050_driver;
